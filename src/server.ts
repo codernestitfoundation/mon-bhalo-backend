ef923b7dcdb669a23a1d090996b5ccfb909f10a3
rokonroni <rokonuzzamanroni@gmail.com>
2026-01-18 22:27:21 +0600

    Project setup completed

---
+++ src/server.ts
@@ -0,0 +1,65 @@
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Server } from "http";
+import mongoose from "mongoose";
+import app from "./app";
+import { envVars } from "./app/config/env";
+
+let server: Server;
+
+
+const startServer = async () => {
+  try {
+    await mongoose.connect(envVars.DB_URL);
+        console.log("Database connected successfully");
+    server = app.listen(envVars.PORT, () => {
+        console.log(`Server is running on Port: ${envVars.PORT}`);
+    });
+  } catch (error: any) {
+    console.error("Server Error", error.message)
+  }
+}
+
+startServer();
+
+
+process.on("SIGINT", () => {
+    console.log("SIGINT signal received, we are closing our server...");
+    if (server) {
+        server.close(() => {
+            process.exit(1);
+        });
+}
+    process.exit(1);
+});
+
+process.on("SIGTERM", () => {
+    console.log("SIGTERM signal received, we are closing our server...");
+    if (server) {
+        server.close(() => {
+            process.exit(1);
+        });
+}
+    process.exit(1);
+});
+
+process.on("unhandledRejection", () => {
+    console.log("Unhandled Rejection is detected, we are closing our server...");
+    if (server) {
+        server.close(() => {
+            process.exit(1);
+        });
+}
+    process.exit(1);
+});
+
+process.on("uncaughtException", () => {
+    console.log("Uncaught Exception is detected, we are closing our server...");
+    if (server) {
+        server.close(() => {
+            process.exit(1);
+        });
+}
+    process.exit(1);
+});
+
